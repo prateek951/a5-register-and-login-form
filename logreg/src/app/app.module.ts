c2e96ce2cd17688f4b245eaed4ddd2ab7752ad19
Prateek Madaan <prince.cenation@gmail.com>
2018-05-01 23:28:07 +0530

    added the animations and the validation for the login and the register pages

--- logreg/src/app/app.module.ts
+++ logreg/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RouterModule, Router} from '@angular/router';
+import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -15,12 +16,15 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       {path: '',redirectTo: '/login',pathMatch: 'full'},
       {path: 'login',component: LoginComponent},
       {path: 'register',component: RegisterComponent},
       {path: '*',redirectTo: '/login'}
-    ])
+    ],{
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
