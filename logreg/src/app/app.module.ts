26ed69941121550dfc5f9d787ebe02047f9fb94c
prateek951 <prince.cenation@gmail.com>
2018-05-01 20:36:57 +0530

    added the skeleton for the login and the register pages

--- logreg/src/app/app.module.ts
+++ logreg/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Router} from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,14 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '',redirectTo: '/login',pathMatch: 'full'},
+      {path: 'login',component: LoginComponent},
+      {path: 'register',component: RegisterComponent},
+      {path: '*',redirectTo: '/login'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
